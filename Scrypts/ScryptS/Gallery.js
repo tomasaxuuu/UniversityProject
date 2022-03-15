012ae6fba4a5989137992c30b0497078e66d463a
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-03-15 20:52:35 +0400

    New JS

--- Scrypts/ScryptS/Gallery.js
+++ Scrypts/ScryptS/Gallery.js
@@ -24,7 +24,7 @@ const sliderBg = new Swiper('.slider_bg', {
 	slidesPerView: 3
 });
 
-sliderMain.controller.control = sliderBg;
+// sliderMain.controller.control = sliderBg;
 // document.querySelectorAll('.slider__item').forEach(item => {
 // 	item.addEventListener('click', event => {
 // 		item.classList.toggle('opened')
