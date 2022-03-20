b93bc325383f61dae16ff1b2ae4a2a1f774c0cde
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-03-21 02:20:15 +0400

    Final block with info

--- Scrypts/ScryptS/Find.js
+++ Scrypts/ScryptS/Find.js
@@ -1,16 +1,16 @@
 let inputText = document.querySelector('.inputText');
     submitForm = document.querySelector('.submitForm');
-    information=document.querySelectorAll('.searchDiv');
+    information = document.querySelectorAll('.searchDiv');
 
-    searchFunction=(text)=>{
+    searchFunction = (text) => {
         information.forEach((element) => {
-            if(element.textContent==text){
-                element.style="animation:animationElement 2s;";
+            if(element.textContent == text){
+                element.style = "animation:animationElement 2s;";
                 console.log(element);
             }
         });
     }
-submitForm.addEventListener('click',(e)=>{
+submitForm.addEventListener('click', (e) => {
     e.preventDefault();
     searchFunction(inputText.value);
 });
