cc3cb75786f90cce0d8ae2a283686737be6402c4
Timur Gayazov <tim.gayazov@gmail.com>
2022-03-29 02:02:01 +0400

    update scrollbar

--- Scrypts/preloader.js
+++ Scrypts/preloader.js
@@ -1,7 +1,7 @@
 document.body.onload = function(){
     setTimeout(function(){
         var preloader = document.getElementById('page-preloader');
-        if(!preloader.classList.contains('done')){
+        if(!preloader.classList.contains('done')){ 
             preloader.classList.add('done');
         }
 
